d7962558352366d34d826705d7bf609588517020
Joha1771 <johasalimov1717@gmail.com>
2025-12-03 05:24:07 -0500

    got some changes

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // For1
-let k = 28;
-let n = 6;
-for (let i = 1; i <= n; i++) {
-  console.log(k);
+
+let k = 13;
+let n = 3;
+
+for(let i = 1; i <= n; i++){
+    console.log(k);
 }
 
 // For2
-let a2 = 41,
-  b2 = 69;
-for (let i = a2; i <= b2; i++) {
-  console.log(i);
+
+let a2 = 18;
+b2 = 36;
+
+for(let i = a2; i <= b2; i++){
+    console.log(i)
 }
 console.log("Count =", b2 - a2 + 1);
 
 // For3
-let a3 = 14,
-  b3 = 20;
-for (let i = b3; i >= a3; i--) {
-  console.log(i);
+
+let a3 = 26;
+b3 = 68;
+
+for(let i = b3-1; i >= a3+1; i--){
+    console.log(i)
 }
-console.log("Count =", b3 - a3 + 1);
+console.log("Count = ", b3 - a3 - 1)
+
 
 // For4
 let price4 = 48000;
@@ -27,77 +34,131 @@ for (let i = 1; i <= 10; i++) {
   console.log(i + " kg narxi = " + price4 * i);
 }
 
+
+// oralarida joy qoldirish uchun chunki google da chlkashib ketyapman will not use it in real projects:
+console.log("")
+
 // For5
 let price5 = 15000;
 for (let i = 0.1; i <= 1; i += 0.1) {
   console.log(i.toFixed(1) + " kg narxi = " + (price5 * i).toFixed(2));
 }
 
-// For6
-let price6 = 45000;
-for (let x = 1.0; x <= 2.0; x += 0.2) {
-  console.log(x.toFixed(1) + " kg narxi = " + price6 * x);
+
+// oralarida joy qoldirish uchun chunki google da chlkashib ketyapman will not use it in real projects:
+console.log("")
+
+
+// For6 
+
+let price6 = 35000;
+
+for(let i = 1.2; i <= 2.0; i = i + 0.2){
+    console.log(i.toFixed(1) + " kg narxi = " + (i * price6).toFixed(2))
 }
 
 // For7
-let A7 = 15,
-  B7 = 28,
-  sum7 = 0;
-for (let i = A7; i <= B7; i++) sum7 += i;
+
+let a7 = 58;
+b7 = 82;
+sum7 = 0;
+
+for(let i = a7; i <= b7; i++){
+    sum7 += i  
+
+    // += буни энди органдим чаптарафдаги static га value кошиларкан  
+
+}
 console.log("Sum =", sum7);
 
 // For8
-let A8 = 15,
-  B8 = 28,
-  mul8 = 1;
-for (let i = A8; i <= B8; i++) mul8 *= i;
-console.log("Multiplication =", mul8);
 
-// For9
-let A9 = 18,
-  B9 = 45,
-  sum9 = 0;
-for (let i = A9; i <= B9; i++) sum9 += i ** 2;
-console.log("Squares Sum =", sum9);
+let a8 = 27;
+b8 = 79;
+sum8 = 1;
+
+for(let i = a8; i <= b8; i++){
+    sum8 *= i
+    // Agar += oxshab chapga ong ni koshish mas kopaytirish uchun 
+    // let da tepada sum8 ga 0 qoysez ishlamas ekan chunki 0 kopaytirivorarkan 
+    // shuning uchun 1 qoyish kerak ekan
+}
+console.log("Sum = ", sum8)
+
+//  For9
+
+let a9 = 15;
+b9 = 29;
+sum9 = 0;
+
+for(let i = a9; i <= b9; i++){
+    sum9 += i**2;
+}
+console.log("Sum = ", sum9 )
 
 // For10
-let n10 = 5,
-  S10 = 0;
-for (let i = 1; i <= n10; i++) S10 += 1 / i;
-console.log("S =", S10);
+
+let n10 = 7;
+s10 = 0;
+
+for(let i = 1; i <= n10; i++){
+    s10 += 1 / i
+}
+console.log("Sum = " ,s10)
 
 // For11
-let n11 = 29,
-  S11 = 0;
-for (let i = n11; i <= 2 * n11; i++) S11 += i ** 2;
-console.log("S =", S11);
 
-// For12
-let n12 = 5,
-  P12 = 1;
-for (let i = 1; i <= n12; i++) P12 *= i;
-console.log("P =", P12);
+let n11 = 13;
+ s11 = 0;
 
-// For13
-let n13 = 10,
-  S13 = 0;
-for (let i = 1; i <= n13; i++) {
-  if (i % 2 === 0) S13 -= 1 / i;
-  else S13 += 1 / i;
+
+ for(let i = n11; i <= 2 * n11; i++){
+    s11 += i**2
+ }
+console.log(s11)
+
+
+// 12
+
+let a12 = 8;
+let s12 = 1;
+
+for(let i = 1; i <= a12; i++) {
+    let term = 1 + i / 10;
+    s12 *= i;
 }
-console.log("Alternating S =", S13);
 
-// For14
-let n14 = 10;
-let S14 = 0;
-for (let i = 1; i <= 2 * n14 - 1; i += 2) {
-  S14 += i ** 3;
+console.log(s12);
+
+
+// 13
+let a13 = 4;
+s13 = 0;
+
+for (let i = 1; i <= a13; i++) {
+    let term = 1 + i / 10;      
+    let sign = (-1) ** (i - 1); 
+    s13 += sign * term;
+    
 }
-console.log("Formula =", S14);
+
+console.log(s13);
+
+// 14 
+
+let a14 = 10;
+let s14 = 0;
+for (let i = 1; i <= 2 * a14 - 1; i = i + 2) {
+  s14 += i ** 3;
+}
+console.log("Formula =", s14);
+
+
 
 // For15
 let a15 = 3,
   n15 = 5,
-  P15 = 1;
-for (let i = 1; i <= n15; i++) P15 *= a15;
-console.log("a^n =", P15);
+  p15 = 1;
+for (let i = 1; i <= n15; i++) 
+    p15 *= a15;
+console.log (p15);
